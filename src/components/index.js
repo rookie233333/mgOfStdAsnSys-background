cdafde116c5048bfa9731e46351418f66345a069
zhu.judong <18780127374@163.com>
2018-04-11 09:13:50 +0800

    路径修改

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import header from './header/header.vue'
 import footer from './footer/footer.vue'
 import main from './main/main.vue'
-import activity from './activity'
+import activity from './activity/index.vue'
 
 export default{
   header,
